e53bb4cc27e766e1662b38c6b4f387d53987ce2b
hyeon-ha <kt01099343122@gmail.com>
2021-12-17 12:23:31 +0900

    qwer

---
+++ exam_webbrowser.py
@@ -0,0 +1,10 @@
+# import webbrowser
+# import numpy as np
+# url='https://www.naver.com'
+# webbrowser.open(url)
+# # a = np.random.randn(100,3)
+# # print(a)
+# #
+# # pd.
+import webbrowser
+webbrowser.get()
